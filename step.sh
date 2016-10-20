6079303ef24037208fb6437ecd6af1f4ea4bd439
Radim Vaculik <radim.vaculik@gmail.com>
2016-10-20 16:05:57 +0200

    Add missing Gemfile

--- step.sh
+++ step.sh
@@ -4,4 +4,8 @@ THIS_SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 set -e
 
+export BUNDLE_GEMFILE="$THIS_SCRIPT_DIR/Gemfile"
+
+bundle install --without test --jobs 20 --retry 5
+
 bundle exec ruby "$THIS_SCRIPT_DIR/step.rb" -a "${apk_path}"
